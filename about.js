4f862fb66afaeacdd4db7e7422777125730a0fa8
Aaron <149936337+avauga03@users.noreply.github.com>
2023-12-30 22:45:23 +0000

    Add files via upload

---
+++ about.js
@@ -0,0 +1,8 @@
+        function confirmSubmission() {
+            // Prevent the default form submit action
+            event.preventDefault();
+            // Display an alert to the user
+            alert('Form submitted successfully!');
+            // If you want to reset the form after submission
+            // document.getElementById('myForm').reset();
+        }
